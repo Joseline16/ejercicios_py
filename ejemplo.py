bad55dba21de806fc81b29ee1b24814f5c0cce11
Joseline16 <jcolanarce16@gmail.com>
2025-01-29 19:10:35 +0100

    primer ejercicio =*

---
+++ ejemplo.py
@@ -0,0 +1,6 @@
+lista_numeros = [1,2,3,4,5,6,7,8,9,10]
+lista_numeros.append(11)#1 Añade el número 11 al final de la lista
+lista_numeros.insert(0,1)#2 Inserta el número 0 al principio de la lista. 
+lista_numeros.pop(5)#3 Elimina el número 5 de la lista
+print(lista_numeros[3:7] )#4
+    #5
